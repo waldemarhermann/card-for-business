4443d32603469824fc9af1f11be51f87067bbd2b
Waldemar Hermann <hermann.waldemar19995@gmail.com>
2022-12-24 15:48:11 +0100

    corrected images in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,9 @@
 function Footer() {
     return (
         <footer className="footer-container">
-           <a href="https://github.com/waldemarhermann"><img src="/twitter-icon.png" alt="twitter-icon" /></a>
-           <a href="https://github.com/waldemarhermann"><img src="/linkedin-icon.png" alt="linkedin-icon" /></a>
-           <a href="https://github.com/waldemarhermann"><img src="/github-icon.png" alt="github-icon" /></a>
+           <img src="twitter-icon.png" alt="twitter-icon" />
+           <img src="linkedin-icon.png" alt="linkedin-icon" />
+           <img src="github-icon.png" alt="github-icon" />
         </footer>
     )
 }
